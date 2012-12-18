016341d47794bf8458ad4a3f6aa798c21cc42b5a
Sami Ylonen <sami.ylonen@gmail.com>
2012-12-18 08:16:51 +0100

    kkkk

--- server/facebookserver.dart
+++
@@ -1,7 +0,0 @@
-part of rtc_server;
-
-class Facebookserver extends Server{
-  void listen([String ip="0.0.0.0", int port=8234, String path="/ws"]) {
-    
-  }
-}
--- server/wheelserver.dart
+++
@@ -1,8 +0,0 @@
-part of rtc_server;
-
-class WheelServer extends Server {
-  
-  void listen([String ip="0.0.0.0", int port=8234, String path="/ws"]) {
-    
-  }
-}
