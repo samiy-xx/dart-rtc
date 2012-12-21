608a516aef7a6c68cc92ce502e094432886d9ce0
Sami Ylonen <sami.ylonen@gmail.com>
2012-12-21 08:19:04 +0100

    dd

--- client/datapeerwrapper.dart
+++ client/datapeerwrapper.dart
@@ -5,27 +5,41 @@ class DataPeerWrapper extends PeerWrapper {
   Logger _log = new Logger();
   
   DataPeerWrapper(PeerManager pm, RtcPeerConnection p) : super(pm, p){
-    _dataChannel = _peer.createDataChannel("1");
-    _dataChannel.on.close.add(onDataChannelClose);
-    _dataChannel.on.open.add(onDataChannelOpen);
-    _dataChannel.on.error.add(onDataChannelError);
-    _dataChannel.on.message.add(onDataChannelMessage);
+    _peer.on.iceCandidate.add(_onIceCandidate);
   }
   
+  void _onIceCandidate(RtcIceCandidateEvent c) {
+    if (c.candidate == null) {
+      // Assume done?
+      
+      _dataChannel = _peer.createDataChannel("1");
+      _dataChannel.on.close.add(onDataChannelClose);
+      _dataChannel.on.open.add(onDataChannelOpen);
+      _dataChannel.on.error.add(onDataChannelError);
+      _dataChannel.on.message.add(onDataChannelMessage);
+    }
+  }
   void onDataChannelOpen(RtcDataChannelEvent e) {
     _log.Debug("DataChannelOpen $e");
+    _log.Debug("DataChannel readystate = ${_dataChannel.readyState}");
+    
+    _dataChannel.send("I HAS OPENED");
   }
   
   void onDataChannelClose(RtcDataChannelEvent e) {
     _log.Debug("DataChannelClose $e");
+    _log.Debug("DataChannel readystate = ${_dataChannel.readyState}");
   }
 
   void onDataChannelMessage(RtcDataChannelEvent e) {
+    
     _log.Debug("DataChannelMessage $e");
+    _log.Debug("DataChannel readystate = ${_dataChannel.readyState}");
   }
 
   void onDataChannelError(RtcDataChannelEvent e) {
     _log.Debug("DataChannelError $e");
+    _log.Debug("DataChannel readystate = ${_dataChannel.readyState}");
   }
   
 }
