5e4d2864f4d3612fe52e335fb769ed69582a85bf
samiy-xx <sami.ylonen@gmail.com>
2013-01-22 20:01:54 +0100

    misc

--- rtc_client/test/packettests.dart
+++ rtc_client/test/packettests.dart
@@ -3,13 +3,17 @@ part of rtc_client_tests;
 class PacketTests {
   run() {
     group('PacketTests', () {
+      String packetId;
+      Packet defaultPacket;
       BinaryPacketHandler handler;
       
       setUp(() {
+        defaultPacket = new ByePacket.With(packetId);
         handler = new BinaryPacketHandler();
       });
       
       tearDown(() {
+        defaultPacket = null;
         handler = null;
       });
       
@@ -18,14 +22,13 @@ class PacketTests {
       });
       
       test("BinaryPacketHandler, When creating ArrayBufferView, Buffer is not null", () {
-        ArrayBufferView view = handler.createBufferView(new ByePacket.With('id'));
+        ArrayBufferView view = handler.createBufferView(defaultPacket);
         expect(view, isNotNull);
       });
       
       test("BinaryPacketHandler, When creating ArrayBufferView, Buffer has correct length", () {
-        Packet p = new ByePacket.With('id');
-        ArrayBufferView view = handler.createBufferView(p);
-        expect(view.byteLength, equals(PacketFactory.get(p).charCodes.length));
+        ArrayBufferView view = handler.createBufferView(defaultPacket);
+        expect(view.byteLength, equals(PacketFactory.get(defaultPacket).charCodes.length));
       });
     });
   }
