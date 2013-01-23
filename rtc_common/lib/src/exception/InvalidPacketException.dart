fdedb0b9ff7a890652e6377d167106cba878a3c3
samiy-xx <sami.ylonen@gmail.com>
2013-01-23 16:15:50 +0100

    oddities

--- rtc_common/lib/src/exception/InvalidPacketException.dart
+++
@@ -1,8 +0,0 @@
-part of rtc_common;
-
-class InvalidPacketException implements Exception{
-  final String msg;
-  final Exception original;
-  const InvalidPacketException([this.msg, this.original]);
-  String toString() => msg == null ? "InvalidPacketException" : msg;
-}
