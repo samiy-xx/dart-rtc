9ebd8e875de04ee65fa688d658c13d842e24fca0
samiy-xx <sami.ylonen@gmail.com>
2013-02-05 18:14:36 +0100

    prepare samples

---
+++ rtc_example/README.md
@@ -0,0 +1,1 @@
+Instructions.. Coming.. some day
---
+++ rtc_example/web/datachannel_filetransfer_sample.dart
@@ -0,0 +1,9 @@
+import "dart:html";
+import '../lib/demo_client.dart';
+import '../../rtc_client/lib/rtc_client.dart';
+import '../../rtc_common/lib/rtc_common.dart';
+
+void main() {
+  
+}
+
